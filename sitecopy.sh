8efc5ae7d7dbf324a98acd7934fb9f242717948e
Enno Rehling <enno@raskesider.no>
2016-10-13 15:37:48 +0200

    ensure cdata where magento uses it.

--- sitecopy.sh
+++ sitecopy.sh
@@ -173,7 +173,8 @@ mv $mgconfig $mgconfig.orig
 ( cat <<XSL
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-<xsl:output method="xml" indent="yes"/>
+<xsl:output method="xml" indent="yes"
+cdata-section-elements="date key table_prefix session_save password dbname username host frontName"/>
 <xsl:template match="@*|node()">
 <xsl:copy>
 <xsl:apply-templates select="@*|node()"/>
