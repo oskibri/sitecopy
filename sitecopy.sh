e17fa6b14f762dee3269555760a14156ece6a9fb
Enno Rehling <enno@raskesider.no>
2016-10-13 12:31:38 +0200

    use awk to replace password in wpconfig, clean up temporary files when done.

--- sitecopy.sh
+++ sitecopy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+wpconfig="public/wp-config.php"
+
 usage() {
   echo "usage: $0 [OPTIONS] user@hostname local-db [remote-db]"
   echo "Options"
@@ -10,22 +12,6 @@ usage() {
 
 abspath() {
   echo $(cd "$(dirname $1)" ;pwd -P)
-}
-
-dbexport() {
-  DBNAME=$1
-  DBUSER=$2
-  DBPASS=$3
-  SOURCE=$4
-  ssh $SOURCE "mysqldump --password=\"$DBPASS\" -u $DBUSER $DBNAME" > $DBNAME.sql
-}
-
-dbimport() {
-  DBNAME=$1
-  DBUSER=$2
-  DBPASS=$3
-  DUMP=$4
-  mysql -u$DBUSER --password="$DBPASS" $DBNAME < $DUMP
 }
 
 AP=`abspath $0`
@@ -79,25 +65,6 @@ if [ -z "$SOURCE" ] ; then
   exit
 fi
 
-cd $HOME
-## begin rsync public folder
-echo "copy site from $SOURCE to $PWD"
-if [ ! -e ~/.ssh/id_sitecopy ] ; then
-  echo "generating new public/private key pair"
-  ssh-keygen -N "" -f ~/.ssh/id_sitecopy
-fi
-echo "adding public key to authorized_keys"
-ssh-copy-id -o StrictHostKeyChecking=no -o PreferredAuthentications=password -i ~/.ssh/id_sitecopy "$SOURCE"
-
-if [ -z "$SSH_AGENT_PID" ]; then
-  echo "starting local ssh-agent"
-  eval `ssh-agent -s`
-fi
-ssh-add ~/.ssh/id_sitecopy
-
-echo "copying public folder from $SOURCE"
-rsync --delete -rauve ssh $SOURCE:public .
-
 
 wp_read_config() {
   ( echo "<?php" ; grep DB_ $1 ; cat <<EOF
@@ -108,51 +75,114 @@ EOF
 ) | php
 }
 
-wp_write_config() {
-  echo "modifying wp-config.php"
+setup_ssh() {
+  if [ ! -e ~/.ssh/id_sitecopy ] ; then
+    echo "generating new public/private key pair"
+    ssh-keygen -N "" -f ~/.ssh/id_sitecopy
+  fi
+  echo "adding public key to authorized_keys"
+  ssh-copy-id -o StrictHostKeyChecking=no -o PreferredAuthentications=password -i ~/.ssh/id_sitecopy "$SOURCE"
+
+  if [ -z "$SSH_AGENT_PID" ]; then
+    echo "starting local ssh-agent"
+    eval `ssh-agent -s`
+  fi
+  ssh-add ~/.ssh/id_sitecopy
 }
 
-## find database credentials
-if [ "$SITE" = "wp" ] ; then
-  wpconfig="public/wp-config.php"
-  if [ -e $wpconfig ] ; then
-    echo "reading credential from $wpconfig"
+config_read() {
+  mkdir -p ~/public
+  if [ "$SITE" = "wp" ] ; then
+    scp $SOURCE:$wpconfig ~/public/
     eval `wp_read_config $wpconfig`
-    echo "name: $SRCDBNAME"
-    echo "user: $SRCDBUSER"
-  else
-    echo "no such file: $wpconfig"
   fi
-fi
+}
 
-if [ ! -z "$SRCDB" ] ; then
-  [ -z "$SRCDBNAME" ] && SRCDBNAME=$SRCDB
-  [ -z "$SRCDBUSER" ] && SRCDBUSER=$SRCDB
-fi
-if [ -z "$SRCDBPASS" ] && [ ! -z "$SRCDBNAME" ]; then
-  echo -n "MySQL Password for $SRCDBNAME:"
-  read -s SRCDBPASS
-  echo
-fi
+dbconf_local() {
+  ## guess/ask local db credentials
+  if [ ! -z "$DSTDB" ] ; then
+    [ -z "$DSTDBNAME" ] && DSTDBNAME=$DSTDB
+    [ -z "$DSTDBUSER" ] && DSTDBUSER=$DSTDB
+  fi
+  if [ -z "$DSTDBPASS" ] && [ ! -z "$DSTDBNAME" ] ; then
+    echo "querying local database information"
+    read -e -p "database name [$DSTDBNAME]:" DSTDB
+    [ -z "$DSTDB" ] || DSTDBNAME=$DSTDB
+    [ -z "$DSTDBUSER" ] && DSTDBUSER=$DSTDB
+    read -e -p "database user [$DSTDBUSER]:" DSTDB
+    [ -z "$DSTDB" ] || DSTDBUSER=$DSTDB
+    read -s -p "database password for $DSTDBUSER@$DSTDBNAME:" DSTDBPASS
+    echo
+  fi
+}
 
-if [ ! -z "$DSTDB" ] ; then
-  [ -z "$DSTDBNAME" ] && DSTDBNAME=$DSTDB
-  [ -z "$DSTDBUSER" ] && DSTDBUSER=$DSTDB
-fi
-if [ -z "$DSTDBPASS" ] && [ ! -z "$DSTDBNAME" ] ; then
-  echo -n "MySQL Password for $DSTDBNAME:"
-  read -s DSTDBPASS
-  echo
-fi
 
-## begin copy database
+dbconf_remote() {
+  ## guess/ask local db credentials
+  if [ ! -z "$SRCDB" ] ; then
+    [ -z "$SRCDBNAME" ] && SRCDBNAME=$SRCDB
+    [ -z "$SRCDBUSER" ] && SRCDBUSER=$SRCDB
+  fi
+  if [ -z "$SRCDBPASS" ] && [ ! -z "$SRCDBNAME" ] ; then
+    echo "querying remote database information"
+    read -e -p "database name [$SRCDBNAME]:" SRCDB
+    [ -z "$RCDB" ] || SRCDBNAME=$SRCDB
+    [ -z "$SRCDBUSER" ] && SRCDBUSER=$SRCDB
+    read -e -p "database user [$SRCDBUSER]:" SRCDB
+    [ -z "$SRCDB" ] || SRCDBUSER=$SRCDB
+    read -s -p "database password for $SRCDBUSER@$SRCDBNAME:" SRCDBPASS
+    echo
+  fi
+}
+
+config_write_wp() {
+mv $wpconfig $wpconfig.orig
+( cat <<AWK
+\$0 ~ "DB_PASSWORD" { sub("$SRCDBPASS","$DSTDBPASS") }
+\$0 ~ "DB_USER" { sub("$SRCDBUSER","$DSTDBUSER") }
+\$0 ~ "DB_NAME" { sub("$SRCDBNAME","$DSTDBNAME") }
+{ print }
+AWK
+) > rules.awk
+awk -f rules.awk < $wpconfig.orig > $wpconfig
+}
+
+config_write() {
+if [ "$SITE" = "wp" ] ; then
+  config_write_wp $wpconfig
+fi
+}
+
+rsync_public () {
+  echo "copying public folder from $SOURCE"
+  rsync --delete -rave ssh $SOURCE:public .
+}
+
+db_transfer(){
+sqlfile=$1
 if [ ! -z "$SRCDBNAME" ] ; then
-  echo "exporting database $SRCDBUSER/$SRCDBNAME to $SRCDBNAME.sql"
-  dbexport $SRCDBNAME $SRCDBUSER $SRCDBPASS $SOURCE
+  echo "exporting database $SRCDBUSER/$SRCDBNAME to $sqlfile"
+  ssh $SOURCE "mysqldump --password=\"$SRCDBPASS\" -u $SRCDBUSER $SRCDBNAME" > $sqlfile
 fi
 if [ ! -z "$DSTDBNAME" ] ; then
-  echo "importing database $DSTDBUSER/$DSTDBNAME from $SRCDBNAME.sql"
+  echo "importing database $DSTDBUSER/$DSTDBNAME from $sqlfile"
   if [ -e $SRCDBNAME.sql ] ; then
-    dbimport $DSTDBNAME $DSTDBUSER $DSTDBPASS $SRCDBNAME.sql && rm -f $SRCDBNAME.sql
+    mysql --user="$DSTDBUSER" --password="$DSTDBPASS" $DSTDBNAME < $sqlfile
   fi
 fi
+}
+
+cleanup() {
+  rm -f sitecopy.sql $wpconfig.orig rules.awk
+}
+
+cd $HOME
+echo "copy site from $SOURCE to $PWD"
+setup_ssh
+config_read
+dbconf_remote
+dbconf_local
+rsync_public
+config_write
+db_transfer sitecopy.sql
+cleanup
